a968bf4038ff88fda12caa5209168673397b853a
muazzzznabil <152073149+muazzzznabil@users.noreply.github.com>
2024-08-15 02:26:39 +0800

    complete appBar? HomePage.dart

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -40,14 +40,60 @@ class _HomePageState extends State<HomePage> {
         Navigator.pushNamed(context, '/prayerTime');
       },
       child: Container(
-        height: 100,
+        height: 200,
         width: 300,
         decoration: BoxDecoration(
           color: Colors.blue,
           borderRadius: BorderRadius.circular(20)
         ),
-        child: const Center(
-          child: Text('Prayer Time'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Text.rich(
+
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+
+                )
+              ],
+
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: Center(
+                child: Text(
+                  'See Prayer Times',
+                  style: TextStyle(
+                    color: Colors.white,
+                      shadows: [
+                        Shadow(
+                            color: Colors.black.withOpacity(0.25),
+                            blurRadius: 10,
+                            offset: Offset(0,4)
+                        )
+                      ]
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xff4c787e),
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20))
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -94,9 +140,73 @@ class _HomePageState extends State<HomePage> {
   AppBar appBar() { //KIV
     return AppBar(
       flexibleSpace: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Text('1 Muharram\n1435H'),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              // child: Text('1 Muharram\n1435H'),
+              child: Text.rich(
+                TextSpan(
+                  text: '1 Muharram\n',
+                  style: TextStyle(
+                    color: Color(0xfff5f2f2),
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.25),
+                        blurRadius: 10,
+                        offset: Offset(0,4)
+                      )
+                    ]
+                  ),
+                  children: <InlineSpan>[
+                    TextSpan(
+                      text: '1453',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                          shadows: [
+                      Shadow(
+                      color: Colors.black.withOpacity(0.25),
+                        blurRadius: 10,
+                        offset: Offset(0,4)
+                    )
+                  ]
+                      )
+                    ),
+                    TextSpan(text: 'H')
+                  ]
+                )
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text.rich(
+                  TextSpan(
+                    text: 'Tuesday\n',
+                    style: TextStyle(
+                      fontSize: 22,
+                        color: Color(0xfff5f2f2),
+                        shadows: [
+                    Shadow(
+                    color: Colors.black.withOpacity(0.25),
+                      blurRadius: 10,
+                      offset: Offset(0,4)
+                  )
+                  ]
+                    ),
+                    children: <InlineSpan>[
+                      TextSpan(
+                        text: '24 October 2024',
+                        style: TextStyle(
+                          fontSize: 10
+                        )
+                      )
+                    ]
+                  )
+              ),
+            )
+          ],
         ),
         height: 250,
         decoration: BoxDecoration(
@@ -104,7 +214,7 @@ class _HomePageState extends State<HomePage> {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color(0xff37553E),
+              Color(0xff37553E).withOpacity(0.9),
               Color(0xff5f936B),
               Color(0xff79BB88)
             ],
