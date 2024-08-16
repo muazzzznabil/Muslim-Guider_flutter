19bc14ea62f54a22e12f646b906c580f32ac9321
muazzzznabil <152073149+muazzzznabil@users.noreply.github.com>
2024-08-16 22:38:00 +0800

    add new font type add background3.png complete tasbih counter widget

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,14 +1,13 @@
+
+
+
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-import 'package:muslim_guider_v1/pages/mosqueLocator.dart';
-import 'package:muslim_guider_v1/pages/tasbihCounter.dart';
+// import 'package:sizes/sizes_helpers.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,10 +17,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  double widgetWidth ()=> MediaQuery.of(context).size.width*0.9;
+
   @override
   Widget build(BuildContext context) {
+
     return  Scaffold(
-    appBar: appBar(),
+    appBar:PreferredSize(
+      preferredSize: Size.fromHeight(100.00),
+      child:appBar(),
+    ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -41,14 +47,16 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+
   GestureDetector prayerTimeWidget() {
+    // double widgetWidth = MediaQuery.of(context).size.width*0.9;
     return GestureDetector(
       onTap: () {
         Navigator.pushNamed(context, '/prayerTime');
       },
       child: Container(
         height: 200,
-        width: 300,
+        width: widgetWidth(),
         decoration: BoxDecoration(
           // color: Colors.blue,
             gradient: LinearGradient(
@@ -79,31 +87,34 @@ class _HomePageState extends State<HomePage> {
                           // mainAxisAlignment: MainAxisAlignment.start,
 
                           children: [
-                            Text.rich( //Now Zuhr
-                           textAlign: TextAlign.start,
-                              TextSpan(
-                                text: 'Now : ',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w200,
-                                  color: Color(0xffF5F2F2),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 26.0),
+                              child: Text.rich( //Now Zuhr
+                                                         textAlign: TextAlign.start,
+                                TextSpan(
+                                  text: 'Now : ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w200,
+                                    color: Color(0xffF5F2F2),
 
-                                  shadows: [
-                                    Shadow(
-                                        color: Colors.black.withOpacity(0.25),
-                                        blurRadius: 10,
-                                        offset: Offset(0,4)
-                                    )
+                                    shadows: [
+                                      Shadow(
+                                          color: Colors.black.withOpacity(0.25),
+                                          blurRadius: 10,
+                                          offset: Offset(4,4)
+                                      )
+                                    ]
+                                  ),
+                                  children: <InlineSpan>[
+                                    TextSpan(
+                                    text: 'Zuhr',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                      )
+                                  )
                                   ]
-                                ),
-                                children: <InlineSpan>[
-                                  TextSpan(
-                                  text: 'Zuhr',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                    )
                                 )
-                                ]
-                              )
+                              ),
                             ),
 
                             Text.rich( // 12:08 PM
@@ -117,7 +128,7 @@ class _HomePageState extends State<HomePage> {
                                           Shadow(
                                               color: Colors.black.withOpacity(0.25),
                                               blurRadius: 10,
-                                              offset: Offset(0,4)
+                                              offset: Offset(4,4)
                                           )
                                         ]
                                     ),
@@ -133,56 +144,62 @@ class _HomePageState extends State<HomePage> {
                                 )
                             ),
 
-                            Text.rich( //Next : Asr
-                                TextSpan(
-                                    text: 'Next : ',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w200,
-                                        color: Color(0xffF5F2F2),
-                                        shadows: [
-                                          Shadow(
-                                              color: Colors.black.withOpacity(0.25),
-                                              blurRadius: 10,
-                                              offset: Offset(0,4)
-                                          )
-                                        ]
-                                    ),
-                                    children: <InlineSpan>[
-                                      TextSpan(
-                                          text: 'Asr',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w500,
-                                          )
-                                      )
-                                    ]
-                                )
+                            Padding(
+                              padding: const EdgeInsets.only(right: 30.0),
+                              child: Text.rich( //Next : Asr
+                                  TextSpan(
+                                      text: 'Next : ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w200,
+                                          color: Color(0xffF5F2F2),
+                                          shadows: [
+                                            Shadow(
+                                                color: Colors.black.withOpacity(0.25),
+                                                blurRadius: 10,
+                                                offset: Offset(4,4)
+                                            )
+                                          ]
+                                      ),
+                                      children: <InlineSpan>[
+                                        TextSpan(
+                                            text: 'Asr',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                            )
+                                        )
+                                      ]
+                                  )
+                              ),
                             ),
 
-                            Text.rich( // 04:08 PM
-                                TextSpan(
-                                    text: '04:08 ',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        color: Color(0xffF5F2F2),
-                                        fontSize: 21,
-                                        shadows: [
-                                          Shadow(
-                                              color: Colors.black.withOpacity(0.25),
-                                              blurRadius: 10,
-                                              offset: Offset(0,4)
-                                          )
-                                        ]
-                                    ),
-                                    children: <InlineSpan>[
-                                      TextSpan(
-                                          text: 'PM',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w200,
-                                              fontSize: 11
-                                          )
-                                      )
-                                    ]
-                                )
+                            Padding(
+                              padding: const EdgeInsets.only(right: 15.0),
+                              child: Text.rich( // 04:08 PM
+                                  TextSpan(
+                                      text: '04:08 ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Color(0xffF5F2F2),
+                                          fontSize: 21,
+                                          shadows: [
+                                            Shadow(
+                                                color: Colors.black.withOpacity(0.25),
+                                                blurRadius: 10,
+                                                offset: Offset(4,4)
+                                            )
+                                          ]
+                                      ),
+                                      children: <InlineSpan>[
+                                        TextSpan(
+                                            text: 'PM',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w200,
+                                                fontSize: 11
+                                            )
+                                        )
+                                      ]
+                                  )
+                              ),
                             ),
                           ],
                         ),
@@ -230,7 +247,7 @@ class _HomePageState extends State<HomePage> {
                         Shadow(
                             color: Colors.black.withOpacity(0.25),
                             blurRadius: 10,
-                            offset: Offset(0,4)
+                            offset: Offset(4,4)
                         )
                       ]
                   ),
@@ -254,7 +271,7 @@ class _HomePageState extends State<HomePage> {
       },
       child: Container(
         height: 100,
-        width: 300,
+        width: widgetWidth(),
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/background/background2.png'),
@@ -263,20 +280,94 @@ class _HomePageState extends State<HomePage> {
           color: Colors.green,
           borderRadius: BorderRadius.circular(20)
         ),
-        child: Row(
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SvgPicture.asset('assets/icons/tasbih2.svg',width: 55,),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Tasbih Counter'),
-                Text('Resume Your Zikr')
-              ],
-            ),
-            Text('6572')
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SvgPicture.asset('assets/icons/tasbih2.svg',width: 55,),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Tasbih Counter',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: Text(
+                            'Resume Your Zikr',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w200,
+                              fontSize: 14,
+
+                            )
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  VerticalDivider(width: 8,thickness: 2,color: Colors.white,),
+                  ShaderMask(
+                    blendMode: BlendMode.srcIn,shaderCallback: (bounds) => LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xffFFFFFF),
+                      Color(0xffFEF0D7),
+                      Color(0xffF7A719),
+                      Color(0xffF7A719),
+                    ],
+                  ).createShader(
+                    Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                  ),
+                    child: Text(
+                      '6572',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black, // Use a base color (white is recommended)
+                          shadows: [
+                            Shadow(
+                                color: Colors.black.withOpacity(0.25),
+                                blurRadius: 20,
+                                offset: Offset(4,4)
+                            )
+                          ]
+                      ),
+                    ),
+                  )
+
+                  // Text(
+                  //     '6572',
+                  //   style: TextStyle(
+                  //     fontSize: 30,
+                  //     fontWeight: FontWeight.w600,
+                  //     color: Gradient.linear(
+                  //         Offset.fromDirection(20),
+                  //         Offset.fromDirection(10),
+                  //         colors:[
+                  //           Color(0xffFEF0D7),
+                  //           Color(0xffF7A719)
+                  //         ]
+                  //     )
+                  //   ),
+                  // ),
+                ],
+              )
+            ],
+          ),
         )
       ),
     );
@@ -289,14 +380,17 @@ class _HomePageState extends State<HomePage> {
       },
       child: Container(
         height: 100,
-        width: 300,
+        width: widgetWidth(),
         decoration: BoxDecoration(
 
             color: Colors.yellow,
             borderRadius: BorderRadius.circular(20)
         ),
-        child: const Center(
-          child: Text('Mosque Locator'),
+        child: Row(
+         children: [
+           Column(),
+           SvgPicture.asset('assets/icons/mosque-islam2.svg')
+         ],
         ),
       ),
     );
@@ -309,13 +403,14 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.only(left: 25.0,top: 25),
               // child: Text('1 Muharram\n1435H'),
               child: Text.rich(
                 TextSpan(
                   text: '1 Muharram\n',
                   style: TextStyle(
                     color: Color(0xfff5f2f2),
+                    fontSize: 18,
                     shadows: [
                       Shadow(
                         color: Colors.black.withOpacity(0.25),
@@ -328,7 +423,7 @@ class _HomePageState extends State<HomePage> {
                     TextSpan(
                       text: '1453',
                       style: TextStyle(
-                        fontSize: 22,
+                        fontSize: 30,
                         fontWeight: FontWeight.w500,
                           shadows: [
                       Shadow(
@@ -345,12 +440,12 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.only(right: 25.0,top: 10),
               child: Text.rich(
                   TextSpan(
                     text: 'Tuesday\n',
                     style: TextStyle(
-                      fontSize: 22,
+                      fontSize: 28,
                         color: Color(0xfff5f2f2),
                         shadows: [
                     Shadow(
@@ -364,7 +459,7 @@ class _HomePageState extends State<HomePage> {
                       TextSpan(
                         text: '24 October 2024',
                         style: TextStyle(
-                          fontSize: 10
+                          fontSize: 13
                         )
                       )
                     ]
