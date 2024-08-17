3b5a317b2ea4b3c10b6bd4d0679c4ee8c7327f64
Farid Izzuddin <faridmarb1903@gmail.com>
2024-08-18 00:12:34 +0800

    deng contactUs.dart 1050

---
+++ lib/pages/ContactUs.dart
@@ -0,0 +1,255 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class contactUs extends StatelessWidget {
+  const contactUs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: Column(
+        children: [
+          prewords(),
+          callUsEmailUs(),
+          SizedBox(height: 30,),
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Contact us on Social Media',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    color: Colors.black
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          GestureDetector(
+            onTap: (){},
+            child: Container(
+              height: 55,
+              width: 295,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Color(0xfff9ce34),
+                      Color(0xffee2a7b),
+                      Color(0xff6228d7),
+                    ],
+                    stops: [0.0,0.5,1.0]
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/instagram-logo 2.svg',
+                  ),
+                  Text(
+                    'muaz.nabil',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                  SvgPicture.asset(
+                      'assets/icons/share 1.svg'
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          GestureDetector(
+            onTap: (){},
+            child: Container(
+              height: 55,
+              width: 295,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Color(0xfff9ce34),
+                      Color(0xffee2a7b),
+                      Color(0xff6228d7),
+                    ],
+                    stops: [0.0,0.5,1.0]
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/instagram-logo 2.svg',
+                  ),
+                  Text(
+                    'fariddinn_',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                  SvgPicture.asset(
+                      'assets/icons/share 1.svg'
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  Row callUsEmailUs() {
+    return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xff00ffff),
+                    Color(0xffffe53b)
+                  ]
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 18,bottom: 18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/call-us.svg',
+                      width: 20,
+                    ),
+                    Text(
+                      'Call us',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.black
+                      ),
+                    ),
+                    Text(
+                      'Our Team is on the line',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 8.5,
+                        color: Colors.black
+                      ),
+                    ),
+                    Text(
+                      'Mon-Fri | 8-17',
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xffffe53b),
+                        Color(0xff00ffff)
+                      ]
+                  )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 18,bottom: 18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/email-us.svg',
+                      width: 20,
+                    ),
+                    Text(
+                      'Email us',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                          color: Colors.black
+                      ),
+                    ),
+                    Text(
+                      'Our Team is on the line',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 8.5,
+                          color: Colors.black
+                      ),
+                    ),
+                    Text(
+                      'Mon-Fri | 8-17',
+                      style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+  }
+
+  Padding prewords() {
+    return Padding(
+          padding: const EdgeInsets.all(30),
+          child: Text(
+            "Don't hesitate to contact us weather you have a suggestion on our improvement"
+                "or a complain to discuss or an issue to solve.",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        'Contact Us',
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 24,
+          color: Color(0xff000000),
+        ),
+      ),
+    );
+  }
+}
