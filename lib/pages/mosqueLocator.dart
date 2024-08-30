e9763215c3b5610dfe3aaf3ba8f7cea1281fe4d8
Farid Izzuddin <faridmarb1903@gmail.com>
2024-08-30 22:59:39 +0800

    deng mosqueLocator.dart 22.59 30/8

--- lib/pages/mosqueLocator.dart
+++ lib/pages/mosqueLocator.dart
@@ -1,142 +1,10 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:muslim_guider_v1/dataProvider/data_provider.dart';
-// import 'package:riverpod/riverpod.dart';
-//
-// import '../model/mosque_model.dart';
-//
-//
-// class mosqueLocator extends ConsumerWidget {
-//   mosqueLocator({super.key});
-//
-//
-//   @override
-//   Widget build(BuildContext context, ref) {
-//     final data = ref.watch(mosqueDataProvider);
-//     return  Scaffold(
-//       body: Container(
-//         decoration: BoxDecoration(
-//           gradient: LinearGradient(
-//             begin: Alignment.topCenter,
-//             end: Alignment.bottomCenter,
-//             colors: [
-//               Color(0xff50775e),
-//               Color(0xff142c28)
-//             ]
-//           ),
-//         ),
-//         child: Column(
-//           children: [
-//             appBar(),
-//             SizedBox(height: 30,),
-//             currentLocation(),
-//             SizedBox(height: 30,),
-//             Container(
-//               child: data.when(
-//                   data: (data){
-//                     List<MosqueLocator> mosqueList = data.map((e) => e).toList();
-//                     return Column(
-//                       children: [
-//                         Expanded(child: ListView.builder(
-//                           itemCount: mosqueList.length,
-//                           itemBuilder: (_,index){
-//                             final mosque = data[index];
-//                             return Card(
-//                               color: Colors.green,
-//                               elevation: 4,
-//                               margin: const EdgeInsets.symmetric(vertical: 10),
-//                               child: ListTile(
-//                                 title: Text(
-//                                   mosque.name,
-//                                   style: const TextStyle(
-//                                     color: Colors.white,
-//                                     fontSize: 15,
-//                                     fontWeight: FontWeight.w500
-//                                   ),
-//                                 ),
-//                               ),
-//                             );
-//                           },
-//                         )
-//                         )
-//                       ],
-//                     );
-//                   },
-//                   error: (err, s) => Text(err.toString()),
-//                   loading: () => const Center(
-//                     child: CircularProgressIndicator(),
-//                   )),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Container currentLocation() {
-//     return Container(
-//             height: 90,
-//             width: 320,
-//             decoration: BoxDecoration(
-//               color: Color(0xffa6c0b3),
-//               borderRadius: BorderRadius.circular(15),
-//             ),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text(
-//                   'Current Location',
-//                   style: TextStyle(
-//                     fontSize: 18,
-//                     fontWeight: FontWeight.w500,
-//                     color: Color(0xffffffff)
-//                   ),
-//                 ),
-//                 SizedBox(height: 5,),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     SvgPicture.asset(
-//                       'assets/icons/redlocation.svg',
-//                       width: 25,
-//                       color: Colors.red,
-//                     ),
-//                     SizedBox(width: 3,),
-//                     Text(
-//                       'Mecca, Saudi Arabia',
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.w500,
-//                         fontSize: 15,
-//                         color: Color(0xff302816),
-//                       ),
-//                     )
-//                   ],
-//                 )
-//               ],
-//             ),
-//           );
-//   }
-//
-//   AppBar appBar() {
-//     return AppBar(
-//             backgroundColor: Colors.transparent,
-//             centerTitle: true,
-//             title: Text(
-//               'Mosque Locator',
-//               style: TextStyle(
-//                 fontWeight: FontWeight.w500,
-//                 fontSize: 24,
-//                 color: Color(0xffffffff)
-//               ),
-//             ),
-//           );
-//   }
-// }
-//
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:muslim_guider_v1/services/geolocator.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -149,13 +17,28 @@ import '../dataProvider/mosque_v2_provider.dart';
 class mosqueLocator extends ConsumerWidget {
   mosqueLocator({super.key});
 
+  final _geolocator = geolocatorFinder();
+  String city = 'loading...';
+  void _getInititalInfo() async {
+    Position position = await _geolocator.getCurrentPosition();
+    Placemark placemark = await _geolocator.getAddressFromLatLng(position);
+    city = placemark.locality.toString();
+  }
+
+
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // Watch the provider to get the list of mosque names
     final mosqueName = ref.watch(mosqueDataProviderV2);
 
+    _getInititalInfo();
     return Scaffold(
-      body: Container(
+      body: RefreshIndicator(
+        onRefresh: () async {
+      ref.refresh(mosqueDataProviderV2);
+    },
+    child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -215,7 +98,7 @@ class mosqueLocator extends ConsumerWidget {
           ],
         ),
       ),
-    );
+    ));
   }
 
   Container currentLocation() {
@@ -242,13 +125,13 @@ class mosqueLocator extends ConsumerWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SvgPicture.asset(
-                'assets/icons/map-pin-color.svg',
+                'assets/icons/redlocation.svg',
                 width: 25,
                 color: Colors.red,
               ),
               SizedBox(width: 3),
               Text(
-                'Mecca, Saudi Arabia',
+                '$city',
                 style: TextStyle(
                   fontWeight: FontWeight.w500,
                   fontSize: 15,
--- lib/services/Mosquev2Service.dart
+++ lib/services/Mosquev2Service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 import 'package:muslim_guider_v1/model/mosquev2_model.dart';
 import 'package:riverpod/riverpod.dart';
@@ -8,20 +9,29 @@ import 'package:muslim_guider_v1/services/geolocator.dart';
 
 class Mosquev2service{
 
-  String lat = '5.2997049';
-  String long = '103.1370079';
-  String key = 'pk.eyJ1IjoibXVhem5hYmlsIiwiYSI6ImNtMDltNXB0czA2YW4ya3B2OHU3b2NheDQifQ.t7Xf0hSVv4uJ8yuhCdcYYA';
   final _geolocator = geolocatorFinder();
 
-
   Future<List<MosqueV2>> getMosques() async{
+
+    Position position = await _geolocator.getCurrentPosition();
+
+    String lat = position.latitude.toString();
+    String long = position.longitude.toString();
+    print('----------------------------------------------------------------------------------');
+    print(lat);
+    print(long);
+
+    //String lat = '5.2997049';
+    //String long = '103.1370079';
+    String key = 'pk.eyJ1IjoibXVhem5hYmlsIiwiYSI6ImNtMDltNXB0czA2YW4ya3B2OHU3b2NheDQifQ.t7Xf0hSVv4uJ8yuhCdcYYA';
+
     final String mapBox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/mosque.json?proximity=$long,$lat&access_token=$key';
     final String foursq = '';
     Response response = await get(Uri.parse(mapBox));
-    print('SHOUDLd IN HERE---------------------------------');
+    print('SHOUDL IN HERE---------------------------------');
     // Check response status
     if (response.statusCode == 200) {
-      print('SUCCESS_--------------------------------------------');
+      print('SUCCESS--------------------------------------------');
       // Assuming the API returns a JSON array of mosque objects
       final List<dynamic> result = jsonDecode(response.body)['features'];
       return result.map((e) => MosqueV2.fromJson(e)).toList();
@@ -29,6 +39,13 @@ class Mosquev2service{
       print('errorrrrr');
       throw Exception(response.reasonPhrase);
     }
+  }
+
+  String getMap(){
+    String name = '';
+
+
+    return name;
   }
 
 
